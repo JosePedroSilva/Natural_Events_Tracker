0beb443d141861adebbbb60ae86f1a1c1eb34f3b
Jose Pedro <josepedro88@live.com.pt>
2021-02-23 17:50:09 +0000

    map displayed and events received on state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+import Map from "./components/Map";
+
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvent = async () => {
+      setLoading(true);
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(false);
+    };
+    fetchEvent();
+  }, []);
+
   return (
-    <div>
-      <h1>Wildfire</h1>
-    </div>
+    <div>{!loading ? <Map eventData={eventData} /> : <h1>Loading</h1>}</div>
   );
 }
 
